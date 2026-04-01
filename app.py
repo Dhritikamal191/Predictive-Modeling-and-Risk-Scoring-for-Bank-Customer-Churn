271fff2781cd280390450ee26a509331d434f74b
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-01 07:47:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -273,7 +273,7 @@ import plotly.graph_objects as go
 from sklearn.metrics import roc_curve, auc
 
 
-y_true = y.values.ravel()
+y= df["Exited"]
 if len(y_true.shape) > 1:
     y_true = y_true.ravel()
 
