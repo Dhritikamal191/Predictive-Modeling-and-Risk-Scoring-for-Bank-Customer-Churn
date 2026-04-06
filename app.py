dd966504a987e049d8efe18645b1f56b8c89eba9
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-06 17:17:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -270,26 +270,6 @@ xaxis_title="Impact on Prediction (SHAP Value)", yaxis_title="Features",template
 
 st.plotly_chart(fig)
 
-import plotly.express as px
-
-explainer = shap.Explainer(model, X_train)
-shap_values = explainer(X_test)
-
-shap_df = pd.DataFrame(shap_values.values, columns=X_test.columns)
-X_df = pd.DataFrame(X_test, columns=X_test.columns)
-
-shap_long = shap_df.melt(var_name="Feature", value_name="SHAP Value")
-value_long = X_df.melt(var_name="Feature", value_name="Feature Value")
-
-shap_long["Feature Value"] = value_long["Feature Value"]
-
-fig = px.strip(shap_long,x="SHAP Value", y="Feature",color="Feature Value", orientation="h", color_continuous_scale="viridis")
-
-fig.update_layout(title="SHAP Feature Impact (Interactive)",xaxis_title="Impact on Prediction",yaxis_title="Features", height=600,template="plotly_white")
-
-fig.update_traces(marker=dict(size=6, opacity=0.7))
-
-st.plotly_chart(fig, use_container_width=True)
 # --------------------------------------------------
 # Customer Feature Visualization
 # --------------------------------------------------
