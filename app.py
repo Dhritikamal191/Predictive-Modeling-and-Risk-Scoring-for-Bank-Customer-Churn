c6a6fdfcf5f7897cc47fb3b830cf14a0caff6d65
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-07 13:30:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ with tab2:
 
      fig=go.Figure(go.Waterfall(name="SHAP", orientation="h", y=shap_df["Feature"],x=shap_df["SHAP Value"], text=shap_df["SHAP Value"].round(3), measure=["relative"]*len(shap_df), increasing=dict(marker=dict(color="#22c55e")), decreasing=dict(marker=dict(color="#ef4444")), totals=dict(marker=dict(color="#3b82f6"))))
 
-     fig.update_layout(title="Feature Contribution to Prediction (SHAP Waterfall)", xaxis_title="Impact on Prediction", yaxis_title="Features", template=plotly_dark", height=500)
+     fig.update_layout(title="Feature Contribution to Prediction (SHAP Waterfall)", xaxis_title="Impact on Prediction", yaxis_title="Features", template="plotly_dark", height=500)
      st.plotly_chart(fig)
 
      colors = shap_df["SHAP Value"]
