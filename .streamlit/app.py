4cd44e95812f600c55989fe689d79a677ffded43
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-09 22:19:11 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -412,7 +412,7 @@ with tab4:
          rec = recall_score(y_test, preds)
          f1 = f1_score(y_test, preds)
     
-         results.append({"Model": name,
+         .append({"Model": name,
          "Accuracy": acc,
          "Recall": rec,
          "F1 Score": f1
