37f2147cd41e0c420b820f9e9c494ca960d5d3b1
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-10 11:53:16 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -399,7 +399,7 @@ with tab4:
      fig=px.bar(df_metrics, x="Model", y=["Accuracy","Recall","F1 Score"], barmode="group", title="Model Performance Comparison")
      st.plotly_chart(fig, use_container_width=True)
 
-     comp_threshold = st.slider("Select Threshold", 0.0, 1.0, 0.12, 0.01, key="comp_slider")
+    
      models_dict={"LR": joblib.load("models/logistic_regression.pkl"),
 "DT": joblib.load("models/decision_tree.pkl"),
 "RF": joblib.load("models/random_forest.pkl"),
