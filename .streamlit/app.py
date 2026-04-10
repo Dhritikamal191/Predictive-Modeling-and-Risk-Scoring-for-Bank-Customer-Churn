119e97552905c72ba8f344763cf7fc9a2038a543
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-10 09:05:59 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -404,11 +404,11 @@ with tab4:
      fig=px.bar(df_metrics, x="Model", y=["Accuracy","Recall","F1 Score"], barmode="group", title="Model Performance Comparison")
      st.plotly_chart(fig, use_container_width=True)
 
-     models_dict={lr_model,feature_names= joblib.load("models/logistic_regression.pkl")
-dt_model,feature_names= joblib.load("models/decision_tree.pkl")
-rf_model,feature_names= joblib.load("models/random_forest.pkl")
-gb_model,feature_names= joblib.load("models/gradient_boosting.pkl")
-xgb_model,feature_names= joblib.load("models/xgboost.pkl")}
+     models_dict={"LR": joblib.load("models/logistic_regression.pkl")
+"DT": joblib.load("models/decision_tree.pkl")
+"RF": joblib.load("models/random_forest.pkl")
+"GB": joblib.load("models/gradient_boosting.pkl")
+"XGB": joblib.load("models/xgboost.pkl")}
      results = []
 
      for name, model in models_dict.items():
