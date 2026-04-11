4e8bf139fdb412d015e4c08c98bc607ac247a636
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-11 14:32:50 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -364,7 +364,7 @@ with tab1:
      plot_results=pd.DataFrame({"Probability":y_prob, "Actual_Status": y_test})
      plot_results["Actual_Status"]=plot_results["Actual_Status"].map({1:"Churned", 0: "Stayed"})
      fig = px.histogram(plot_results, x="Probability", nbins=30, color="Actual_Status", title="Distribution of Customer Churn Probability (Test Set)", color_discrete_sequence=["#6366f1","#f43f5e"], barmode="overlay")
-     fig.update_traces(opacity=0.7)
+     fig.update_traces(opacity=0.5)
      fig.update_layout(bargap=0.1, legend_title_text="Customer Status",template="plotly_dark",paper_bgcolor="rgba(0,0,0,0)",plot_bgcolor="rgba(0,0,0,0)")
      st.plotly_chart(fig, use_container_width=True)
      
