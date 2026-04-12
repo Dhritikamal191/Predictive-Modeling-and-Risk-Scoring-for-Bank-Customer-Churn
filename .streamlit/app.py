376cbd11eb9e9ad62bf6c6e564b1b177d3c98c02
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-12 11:51:13 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -284,7 +284,8 @@ else:
 
 col1, col2, col3 = st.columns(3)
 
-with col1:  
+with col1:  
+
      st.metric("🧑‍🧑‍🧒‍🧒 Churn Probability", round(prob,3))
 
 with col2:
@@ -376,25 +377,12 @@ with tab1:
                 x_range = np.linspace(0, 1, 500)
                 y_range = kde(x_range)
          
-                fig.add_trace(go.Scatter(
-                x=x_range, 
-                y=y_range,
-                mode='lines',
-                name=label,
-                fill='tozeroy',  line=dict(color=color, width=2),
-                fillcolor=color,
-                opacity=0.5))
+                fig.add_trace(go.Scatter(x=x_range, y=y_range, mode='lines', name=label, fill='tozeroy',  line=dict(color=color, width=2), fillcolor=color, opacity=0.5))
          m_val = np.mean(kde_probs)
-         fig.add_vline(x=m_val,   line_dash="dash", line_color="red")
+         fig.add_vline(x=m_val, line_dash="dash", line_color="red")
          fig.add_annotation(x=m_val, text=f"Mean: {m_val:.2f}", showarrow=False, yshift=10)
 
-         fig.update_layout(
-         title="Probability Distribution (Churned vs. Stayed)",
-         xaxis_title="Probability",
-         yaxis_title="Density",       template="plotly_dark",paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)",
-         xaxis=dict(range=[0, 1]),
-         legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99)
-         )
+         fig.update_layout(title="Probability Distribution (Churned vs. Stayed)", xaxis_title="Probability", yaxis_title="Density", template="plotly_dark",paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", xaxis=dict(range=[0, 1]), legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99))
 
          return fig
 
