1489e95d2db67cc1acedff670da829ec3eec11a1
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-22 11:25:53 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -496,7 +496,7 @@ with tab1:
               shap_df_top=shap_df.head(top_n)
               
               fig = px.bar(shap_df_top,x="SHAP Value",y="Feature",orientation="h",color="SHAP Value",color_continuous_scale="RdBu",title="Feature Impact on Prediction")
-              fig.update_layout(template="plotly_dark",xaxis_title="Impact on Prediction",yaxis_title="Features",yaxis=dict(autorange="reversed"),coloraxis_color=dict(title="SHAP Value"),margin=dict(l=50,r=50,t=50,b=50),height=400,font=dict(color="white"), legend=dict(font=dict(color="white")),paper_bgcolor="rgba(0,0,0,0)",plot_bgcolor="rgba(0,0,0,0)")
+              fig.update_layout(template="plotly_dark",xaxis_title="Impact on Prediction",yaxis_title="Features",yaxis=dict(autorange="reversed"),coloraxis_colorbar=dict(title="SHAP Value"),margin=dict(l=50,r=50,t=50,b=50),height=400,font=dict(color="white"), legend=dict(font=dict(color="white")),paper_bgcolor="rgba(0,0,0,0)",plot_bgcolor="rgba(0,0,0,0)")
               fig.update_traces(text=shap_df_top["SHAP Value"].round(3),textposition="outside")
               st.plotly_chart(fig, use_container_width=True)
           except Exception as e:
