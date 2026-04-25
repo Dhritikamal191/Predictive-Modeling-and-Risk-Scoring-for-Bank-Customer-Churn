c7e8f6c86d2a3043dc806cd3182fa44e94a585c7
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-25 13:42:47 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -374,12 +374,11 @@ with col5:
 st.divider()
 
 scenario_df = input_df.copy()
-scenario_encoded=scenario_df.reindex(columns=columns, fill_value=0)
 scenario_df["NumOfProducts"] = products
 scenario_df["IsActiveMember"] = active_member
 scenario_df["HasCrCard"] = has_card
 
-new_probability = model.predict_proba(input_encoded)[0][1]
+new_probability = model.predict_proba(scenario_df)[0][1]
 new_risk = new_probability * 100
 
 tab1, tab2, tab3, tab4= st.tabs(["Customer Risk Calculator","Feature Importance","ROC and PDP","Model Comparison"])
