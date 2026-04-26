a73ded0ea040e65c7fc08b408781b410188ad953
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-26 22:10:24 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -387,7 +387,7 @@ with tab1:
      col1,col2=st.columns(2)
 
      with col1:
-          diff=new_risk-new_score
+          diff=new_risk-risk_score
           if abs(diff)<0.1:
              diff=0
           fig1 = go.Figure(go.Indicator(mode="gauge+number+delta", value=new_risk, title={'text': "Customer Churn Risk (%)",'font': {'size': 20}}, delta={'reference': risk_score,'relative':False,'valueformat':"+.2f",'increasing': {'color': "red"},'decreasing':{'color':"green"}},gauge={'axis': {'range': [0, 100], 'tickwidth': 1},'bar': {'color':"#2563eb", 'thickness': 0.25},'bgcolor':"#111827",'borderwidth':2,'bordercolor':"#374151",'steps': [{'range': [0, 40], 'color': "#16a34a"},{'range': [40, 70],'color':"#ca8a04"},{'range': [70, 100], 'color':"#dc2626"}],'threshold': {'line': {'color':"black", 'width':4},'thickness':0.75,'value':new_risk}}))
