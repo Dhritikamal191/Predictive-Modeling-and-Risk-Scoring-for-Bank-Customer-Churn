d25f0be7761bdbfbaea4be7e9bb94d2210638076
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-26 21:06:48 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -444,8 +444,8 @@ with tab1:
               fig.update_layout(template="plotly_dark")
               fig.update_yaxes(autorange="reversed")
               st.plotly_chart(fig)
-          else:
-               st.warning("Explainability not available for this model")
+         else:
+              st.warning("Explainability not available for this model")
       except Exception as e:
             st.error(f"SHAP Error: {e}")
      top_n=10
