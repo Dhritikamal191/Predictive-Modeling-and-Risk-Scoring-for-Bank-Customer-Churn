44614732007d893b0fc488580345a9d8eea7c03c
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-28 13:09:28 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -488,17 +488,16 @@ with tab1:
                 x_range = np.linspace(0, 1, 500)
                 y_range = kde(x_range)
          
-          fig.add_trace(go.Scatter(x=x_range, y=y_range, mode='lines', name=label, fill='tozeroy',  line=dict(color=color, width=2), fillcolor=color, opacity=0.5))
-          m_val = np.mean(kde_probs)
-          fig.add_vline(x=m_val, line_dash="dash", line_color="red")
-          fig.add_annotation(x=m_val, text=f"Mean: {m_val:.2f}", showarrow=False, yshift=10)
-          fig.update_layout(xaxis_title="Probability", yaxis_title="Density",font=dict(color="white"), template="plotly_dark",paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", xaxis=dict(range=[0, 1]), legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99,font=dict(color="white")))
-          fig.update_xaxes(showgrid=False)
-          fig.update_yaxes(showgrid=False)
-          return fig
-          st.plotly_chart(render_comparison_kde(model, X, y_test), use_container_width=True)
-          
-         
+         fig.add_trace(go.Scatter(x=x_range, y=y_range, mode='lines', name=label, fill='tozeroy',  line=dict(color=color, width=2), fillcolor=color, opacity=0.5))
+         m_val = np.mean(kde_probs)
+         fig.add_vline(x=m_val, line_dash="dash", line_color="red")
+         fig.add_annotation(x=m_val, text=f"Mean: {m_val:.2f}", showarrow=False, yshift=10)
+         fig.update_layout(xaxis_title="Probability", yaxis_title="Density",font=dict(color="white"), template="plotly_dark",paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", xaxis=dict(range=[0, 1]), legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99,font=dict(color="white")))
+         fig.update_xaxes(showgrid=False)
+         fig.update_yaxes(showgrid=False)
+         return fig
+         st.plotly_chart(render_comparison_kde(model, X, y_test), use_container_width=True)
+        
 with tab2:
      # --------------------------------------------------
      # Feature Importance Dashboard
