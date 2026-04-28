ed811480fd30a9e050075b87a862a7990133c43b
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-28 11:53:12 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -392,7 +392,7 @@ with tab1:
      with col1:
           fig1 = go.Figure(go.Indicator(mode="gauge+number+delta", value=new_risk, title={'text': "Customer Churn Risk (%)",'font': {'size': 20}}, delta={'reference': risk_score,'relative':False,'valueformat':"+.2f",'increasing': {'color': "red"},'decreasing':{'color':"green"}},gauge={'axis': {'range': [0, 100], 'tickwidth': 1},'bar': {'color':"#2563eb", 'thickness': 0.25},'bgcolor':"#111827",'borderwidth':2,'bordercolor':"#374151",'steps': [{'range': [0, 40], 'color': "#16a34a"},{'range': [40, 70],'color':"#ca8a04"},{'range': [70, 100], 'color':"#dc2626"}],'threshold': {'line': {'color':"black", 'width':4},'thickness':0.75,'value':new_risk}}))
 
-          fig1.update_layout(title="Customer Churn Risk (%)",x=0.5, xanchor="center",font=dict(size=20, color="white"),height=400,margin=dict(l=20, r=20, t=50, b=20),template="plotly_dark",paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
+          fig1.update_layout(title_x=0.3,height=400,margin=dict(l=20, r=20, t=50, b=20),template="plotly_dark",paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
 
           st.plotly_chart(fig1)
 
